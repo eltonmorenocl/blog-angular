d25a399a980db063e73ea67add2c9e5abfcb0b73
Elton <eltonmorenocl@gmail.com>
2023-10-02 17:42:32 -0300

    feat: cria content para pagina de conteudo

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "Framework Angular",
+    "description": "Introdução ao angular - HELLO WORLD! Angular é uma plataforma de aplicações web de código-fonte aberto e front-end baseado em TypeScript liderado pela Equipe Angular do Google e por uma comunidade de indivíduos e corporações. Angular é uma reescrita completa do AngularJS, feito pela mesma equipe que o construiu.",
+    "photoCover":"https://angular.io/assets/images/logos/angular/angular.png"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
+    "title": "Framework Angular",
     "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "photoCover":"https://swansoftwaresolutions.com/wp-content/uploads/2019/04/The-Fundamentals-of-AngularJS-Framework-in-Programming-1024x576.jpg"
+  },
+  {
+    "id":"3",
+    "title": "One framework mobile & desktop",
+    "description": "bla blabla",
+    "photoCover":"https://lh6.googleusercontent.com/rIfflcoaDLMz3am1x4N28xcDuemiRJZQkwSROydZEDbRzc8Tdy7CZfyr-FiVNtA7dOEjhibtFTbIwC8G-IGZXbTr6gf8A2sr2u5jR6XUbhst_ANQ1RbvVjv0XNqikvT2AdsLZkw"
+  },
+  {
+    "id":"4",
+    "title": "What is the Angular Framework",
+    "description": "bla blabla",
+    "photoCover":"https://static.wixstatic.com/media/a59c81_459197f42c634538b91560ade863e719~mv2.jpg/v1/fill/w_640,h_336,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/a59c81_459197f42c634538b91560ade863e719~mv2.jpg"
   }
 ]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {dataFake} from '../../data/dataFake'
 
 @Component({
   selector: 'app-content',
@@ -7,12 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContentComponent implements OnInit {
 
-  photoCover:string = 'https://miro.medium.com/v2/resize:fit:640/0*1yo17Ov28sa7p33N'
-  contentTitle:string = 'New free, interactive Angular course for beginners on Sololearn'
-  contentDescription:string = 'We are delighted to announce that we’ve collaborated with the online interactive code learning platform Sololearn to develop an introductory course to Angular. The course is available for free on Sololearn’s platform to anyone who wants to learn Angular With hands-on coding exercises and projects, learners get practical experience working with real examples of Angular code. They’ll learn the fundamentals of Angular, such as components, routing, and more.'
-  constructor() { }
+  photoCover:string = ''
+  contentTitle:string = ''
+  contentDescription:string = ''
+  private id:string | null = "0"
+
+  constructor(
+    private route:ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
-  }
+    this.route.paramMap.subscribe( value =>
+      this.id = value.get("id")
+     )
+ 
+     this.setValuesToComponent(this.id)
+   }
+ 
+   setValuesToComponent(id:string | null){
+     const result = dataFake.filter(article => article.id == id)[0]
+ 
+     this.contentTitle = result.title
+     this.contentDescription = result.description
+     this.photoCover = result.photoCover
+   }
 
 }
